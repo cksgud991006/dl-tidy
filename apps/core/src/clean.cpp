8d7f724311cb214941add2ef9294dbabb23c8667
Chanhuyng Shim <cksgud991006@gmail.com>
2026-05-06 20:53:29 -0400

    refactor complete

--- apps/core/src/clean.cpp
+++ apps/core/src/clean.cpp
@@ -4,14 +4,14 @@
 #include "log.h"
 
 std::uintmax_t cleanUp(std::filesystem::path path) {
-
-    // manually delete files and prevent dealing with removal of downloads directory
+    
     std::uintmax_t nums = 0;
-    std::uintmax_t num;
     std::error_code ec;
 
+    log_debug(std::format("Cleaning up {}", path.string()));
+
     try {
-        nums = std::filesystem::remove_all(path, ec);
+        nums = std::filesystem::remove_all(path);
     } catch (const std::filesystem::filesystem_error& e) {
         log_error(e.what());
         log_error(e.path1().string());
